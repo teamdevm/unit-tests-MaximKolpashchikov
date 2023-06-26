ee3beb8aba91aa9b1f0227809bf80c15081b622e
Максим Колпащиков <134125715+MaximKolpashchikov@users.noreply.github.com>
2023-06-26 07:28:46 +0000

    бла-бла

--- app/converter.py
+++ app/converter.py
@@ -4,10 +4,13 @@ class Converter:
     def get_exchange_rate(self, currency: str) -> float:
         # Метод возвращает курс указанной валюты
         # url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-        
-        return None 
+        if (currency == 'USD'):
+            return 100
+        elif (currency == 'EUR'):
+            return 300
+        elif (currency == 'GBR'):
+            return 200
 
     def convert_bitcoins(self, currency: str, coins: int) -> float:
         # Метод возвращает сколько стоит в указанной валюте заданное количество биткоинов
-        
-        return None
+        return self.get_exchange_rate(currency) * coins
